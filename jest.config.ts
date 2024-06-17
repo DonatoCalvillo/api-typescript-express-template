092b8ab2ea2edba91d2edd62c3f897394fde7dc6
Edgar Donato <donato.calvillo@cait.mx>
2024-06-17 14:22:15 -0600

    feat: add alias

--- jest.config.ts
+++ jest.config.ts
@@ -1,58 +1,5 @@
-// import { Config } from "@jest/types";
-// /** @type {import('ts-jest/dist/types'.InitialOptionsTsJest)} */
-// const config: Config.InitialOptions = {
-//   // All imported modules in your tests should be mocked automatically
-//   automock: true,
-
-//   // Indicates whether the coverage information should be collected while executing the test
-//   collectCoverage: true,
-
-//   // Automatically clear mock calls, instances, contexts and results before every test
-//   clearMocks: true,
-
-//   // An array of glob patterns indicating a set of file for which coverage information should be collected
-//   collectCoverageFrom: [
-//     "src/**/*.{js,jsx}",
-//     "src/**/*.{js,jsx}",
-//     "!**/node_modules/**",
-//   ],
-
-//   coverageDirectory: "coverage",
-
-//   // A preset that is used as a base for jests configuration
-//   preset: "ts-jest",
-
-//   // The test environment that will be used for testing
-//   testEnvironment: "node",
-
-//   // Indicates whether each individual test should be reported during the run
-//   verbose: true,
-
-//   testTimeout: 100000,
-//   transform: {
-//     "^.+\\.jsx?$": "babel-jest",
-//     "^.+\\.tsx?$": [
-//       "ts-jest",
-//       {
-//         useESM: true,
-//       },
-//     ],
-//   },
-//   testMatch: [
-//     "**/tests/**/*.+(ts|tsx|js)",
-//     "**/(?!!+)(*.)+(spec|test).+(ts|tsx|js)",
-//     "**/tests/e2e/**/*.e2e.spec.+(ts|tsx|js)",
-//   ],
-//   moduleNameMapper: {
-//     "^@/(.*)$": "<rootDir>/src/$1",
-//   },
-//   modulePaths: ["<rootDir>/src/"],
-//   transformIgnorePatterns: ["/node_modules/"],
-//   testPathIgnorePatterns: ["<rootDir>/node_modules", "<rootDir>/dist"],
-//   extensionsToTreatAsEsm: [".ts"],
-// };
-
-// export default config;
+import { pathsToModuleNameMapper } from "ts-jest";
+import { compilerOptions } from "./tsconfig.json";
 
 module.exports = {
   verbose: true,
@@ -73,13 +20,12 @@ module.exports = {
     "**/(?!!+)(*.)+(spec|test).+(ts|tsx|js)",
     "**/tests/e2e/**/*.e2e.spec.+(ts|tsx|js)",
   ],
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
-  },
   modulePaths: ["<rootDir>/src/"],
   transformIgnorePatterns: [
-    "/node_modules/", // Agrega las dependencias que requieres transformar aquí
+    "/node_modules/",
   ],
   testPathIgnorePatterns: ["<rootDir>/node_modules", "<rootDir>/dist"],
   extensionsToTreatAsEsm: [".ts"],
+  modulePath: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths)
 };
