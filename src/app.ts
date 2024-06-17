7dcc52e2e49684f9de09567daf6f2d9875bd112e
Edgar Donato <donato.calvillo@cait.mx>
2024-06-17 02:03:44 -0600

    refactor: app

--- src/app.ts
+++ src/app.ts
@@ -1,53 +1,53 @@
-import express, { type Application } from 'express'
-import cors from 'cors'
+import express, { type Application } from "express";
+import cors from "cors";
 
-import swaggerUI from 'swagger-ui-express'
-import swaggerJsDoc from 'swagger-jsdoc'
+import swaggerUI from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
 
-import { swaggerConfiguration } from './config/swagger'
-import { variables } from './config/variables'
+import { swaggerConfiguration } from "./config/swagger";
+import { variables } from "./config/variables";
 
-import { type Logger } from './modules/shared/domain/logger'
-import { container } from './modules/shared/infrastructure/container'
+import { type Logger } from "./modules/shared/domain/logger";
+import { container } from "./modules/shared/infrastructure/container";
 
-import { healthRouter } from './modules/health/infrastructure/api/routes/health-router'
+import { healthRouter } from "./modules/health/infrastructure/api/routes/health-router";
 
 export class App {
-  _app: Application
-  _port: number
-  _logger: Logger
+  private readonly app: Application;
+  private readonly port: number;
+  private readonly logger: Logger;
 
-  constructor () {
-    this._app = express()
+  constructor() {
+    this.app = express();
 
-    this._port = variables.get('server.port')
+    this.port = variables.get("server.port");
 
-    this._logger = container.resolve<Logger>('logger')
+    this.logger = container.resolve<Logger>("logger");
 
-    this.middlewares()
+    this.middlewares();
 
-    this.router()
+    this.router();
   }
 
-  middlewares (): void {
+  middlewares(): void {
     // CORS
-    this._app.use(cors())
+    this.app.use(cors());
 
     // Accept JSON
-    this._app.use(express.json({ limit: '500kb' }))
+    this.app.use(express.json({ limit: "500kb" }));
   }
 
-  router (): void {
-    this._app.use('/api/health', healthRouter)
+  router(): void {
+    this.app.use("/api/health", healthRouter);
 
-    const specs = swaggerJsDoc(swaggerConfiguration)
-    this._app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(specs))
+    const specs = swaggerJsDoc(swaggerConfiguration);
+    this.app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(specs));
   }
 
-  listen (): void {
-    this._app.listen(this._port, () => {
-      this._logger.info(`[APP] - Started application on port ${this._port}`)
-      this._logger.info(`[APP] - Environment: ${variables.get('env')}`)
-    })
+  listen(): void {
+    this.app.listen(this.port, () => {
+      this.logger.info(`[APP] - Started application on port ${this.port}`);
+      this.logger.info(`[APP] - Environment: ${variables.get("env")}`);
+    });
   }
 }
